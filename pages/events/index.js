a1eb0804013571373ccaf87c6b27f9fb490df49b
therreth <thenry2@aceproject.space>
2023-01-20 13:59:04 -0600

    82. Filtering Events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,15 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/event-search";
+import { Fragment } from "react";
 function AllEventsPage() {
   const events = getAllEvents();
 
   return (
-    <div>
+    <Fragment>
+      <EventSearch />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 }
 
